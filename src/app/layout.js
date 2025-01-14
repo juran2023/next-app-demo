018f71af1b743e0c94c3aabd8db051b3f6009193
JuRan <juranfuture@gmail.com>
2025-01-14 08:28:33 +0800

    feat: finish static contents

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,18 @@
+import { Navigation } from '@/components/Navigation'
+
 export const metadata = {
   title: 'Full-Stack Next.js Blog',
   description: 'A blog about React and Next.js',
 }
 
 export default function RootLayout({ children }) {
+  const user = { username: 'dan' }
   return (
     <html lang='en'>
       <body>
+        <nav>
+          <Navigation username={user?.username} />
+        </nav>
         <main>{children}</main>
       </body>
     </html>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types'
+import Link from 'next/link'
 
-export function Post({_id, title, author}) {
+export function Post({ _id, title, author }) {
   console.log(`_id: ${_id}`)
   console.log(`title: ${title}`)
   console.log(`author: ${author}`)
   return (
     <article>
-      <h3>{title}</h3>
+      <h3>
+        <Link href={`/posts/${_id}`}>{title}</Link>
+      </h3>
       <em>
         Written by <strong>{author.username}</strong>
       </em>
