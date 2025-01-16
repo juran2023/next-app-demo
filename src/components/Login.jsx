46d8b3d86916a023f662f8bad9bfe45afd50ec6f
jufj <juranfuture@gmail.com>
2025-01-16 18:27:32 +0800

    fix: change login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,12 @@
-export function Login() {
+'use client'
+import { useFormState } from "react-dom"
+import PropTypes from 'prop-types'
+
+export function Login({loginAction}) {
+  const [state, formAction] = useFormState(loginAction, {})
+
   return (
-    <form>
+    <form action={formAction}>
       <div>
         <label htmlFor='username'>Username: </label>
         <input type='text' name='username' id='username' />
@@ -12,6 +18,11 @@ export function Login() {
       </div>
       <br />
       <input type='submit' value='Log In' />
+      {state.error ? <strong>Error logging in: {state.error}</strong> : null}
     </form>
   )
+}
+
+Login.propTypes = {
+  loginAction: PropTypes.func.isRequired
 }
